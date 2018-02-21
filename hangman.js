495339efbb6da0a9dd0b35377fa8805674077485
nsch1 <nick.schooneman1@gmail.com>
2018-02-21 16:17:56 +0100

    Add function to show letters of correct guesses

--- hangman.js
+++ hangman.js
@@ -3,3 +3,10 @@ function wrongGuessCount(word, guesses) {
 }
 
 console.log('test wrong guesses: ', wrongGuessCount('pkrthhn', ['e', 'd', 'x', 'o']))
+
+function showGuess(word, guesses) {
+  return word.split('').map(letter => (guesses.indexOf(letter) < 0) ? "_" : letter).join(" ");
+}
+
+console.log('test show guess 1:', showGuess('hello', ['l']) === '_ _ l l _')
+console.log('test show guess 2:', showGuess('hello', ['l', 'a', 'e']) === '_ e l l _')

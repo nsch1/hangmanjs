ace7d9e1fa49d123ec7fb42ecc29405131566a9b
nsch1 <nick.schooneman1@gmail.com>
2018-02-21 23:16:57 +0100

    Working hangman console game

--- hangman.js
+++ hangman.js
@@ -1,27 +1,22 @@
 const readline = require('readline')
 const rl = readline.createInterface({input:process.stdin, output:process.stdout})
+const randomWord = require('random-words')
 
 function wrongGuessCount(word, guesses) {
   return guesses.filter(guess => word.indexOf(guess) < 0).length
 }
 
-console.log('test wrong guesses: ', wrongGuessCount('pkrthhn', ['e', 'd', 'x', 'o']))
-
 function showGuess(word, guesses) {
   return word.split('').map(letter => (guesses.indexOf(letter) < 0) ? "_" : letter).join(" ");
 }
-
-console.log('test show guess 1:', showGuess('hello', ['l']) === '_ _ l l _')
-console.log('test show guess 2:', showGuess('hello', ['l', 'a', 'e']) === '_ e l l _')
 
 function isWinner(word, guesses) {
     return showGuess(word, guesses).indexOf("_") === -1;
 }
 
-console.log('test winner 1:', !isWinner('hello', ['e', 'x']))
-console.log('test winner 2:', isWinner('hello', ['o', 'l', 'e', 'h']))
-
 function next(word, guesses) {
+    Object.freeze(guesses)
+    console.clear()
     console.log(showGuess(word, guesses))
     // check if lost
     if (wrongGuessCount(word, guesses) >= 6) {
@@ -37,11 +32,13 @@ function next(word, guesses) {
       return
     }
     // ask for the next letter
+    console.log(`Previous guesses: ${guesses.join(', ')}.`)
+    console.log(`Wrong guesses: ${wrongGuessCount(word, guesses)}`)
     rl.question('next letter? ', answer => {
-        console.log('player wrote:', answer)
+        console.log(`Previous guesses: ${guesses.concat(answer).join(', ')}.`)
         // do something with answer
-        next(word, guesses.concat(answer))
+        next(word, (answer === '') ? guesses : guesses.concat(answer))
     })
 }
 
-next("hello", [])
+next(randomWord(), [])
